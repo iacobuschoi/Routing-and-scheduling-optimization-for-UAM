685b5716e6567d6982385d54371977e3381a434d
iacobuschoi <iacobuschoi@gmail.com>
2025-11-03 22:46:06 +0900

    request class

---
+++ request.py
@@ -0,0 +1,10 @@
+class Request:
+    def __init__(self):
+        self.positionVectorList = []
+    
+    def appendPositionVector(self, positionVector):
+        self.positionVectorList.append(positionVector)
+    
+    def calcCost(self):
+        minD = min([len(i) for i in self.positionVectorList])
+        return [minD/len(i) for i in self.positionVectorList]
